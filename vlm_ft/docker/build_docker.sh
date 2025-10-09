58f0c0ec2a507bec2bc78e8d992aa1a9bcc42d22
Cristiano Battistini <cristianobattistini09@gmail.com>
2025-10-09 14:31:23 +0200

    trying to fix docker

--- vlm_ft/docker/build_docker.sh
+++ vlm_ft/docker/build_docker.sh
@@ -3,12 +3,14 @@ set -e
 
 IMAGE="battistini/oxe-vlm:cuda12.1-py23"
 
-cd "$(dirname "$0")"/..
+# usa la cartella del Dockerfile come contesto
+cd "$(dirname "$0")"   # ora sei in vlm_ft/docker
 
-# Passa UID e GID al build
 docker build \
   --build-arg USER_UID=$(id -u) \
   --build-arg USER_GID=$(id -g) \
-  -t "$IMAGE" -f docker/Dockerfile .
+  -t "$IMAGE" \
+  -f Dockerfile \
+  .
 
 echo "✅ Immagine creata: $IMAGE"
